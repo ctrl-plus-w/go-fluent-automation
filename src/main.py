5e36a6192c8fc17d487fc466c92b42ab81299301
Lukas Laudrain <lukas.ldrn@gmail.com>
2023-11-15 20:58:40 +0100

    :truck: Added a count parameter

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 """Main"""
 import sys
 import re
+import chalk
 
 from datetime import datetime
 
@@ -21,11 +22,26 @@ def main():
     logger = Logger("MAIN", f"{logs_directory}/logs.txt")
 
     if "--auto-run" in sys.argv:
+        param_index = sys.argv.index("--auto-run")
+
+        if len(sys.argv) - 1 <= param_index:
+            raise ValueError("Missing --auto-run count property.")
+
+        count = int(sys.argv[param_index + 1])
+        logger.info(f"Starting the automaion operations for {count} activities.")
+
         scraper = Scraper(logger)
 
-        activities = scraper.retrieve_activities(10)
+        activities = scraper.retrieve_activities(count)
 
-        for activity in activities:
+        for i, activity in enumerate(activities):
+            logger.info(
+                chalk.bold(
+                    chalk.cyan(
+                        f"\nStarting the activity with url : '{activity.url}' ({i+1}/{len(activities)})"
+                    )
+                )
+            )
             scraper.do_activity(activity)
 
         scraper.driver.quit()
